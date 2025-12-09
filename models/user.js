6db6308c7cd5a4e879766a35f6afa8d95e943079
Ami <ami.dorsey@me.com>
2025-12-09 18:42:10 -0500

    update

--- models/user.js
+++ models/user.js
@@ -1,6 +1,38 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: String,
+    enum: ['fruit', 'vegetable', 'grain', 'dairy', 'protein','other'],
+    required: true
+  },
+  calories: {
+    type: Number,
+    min: 0
+  },
+  nutrients: {
+    protein: {type: Number, default: 0},
+    carbs: {type: Number, default: 0},
+    fat: {type: Number, default: 0},
+    fiber: {type: Number, default: 0},
+  },
+  ServingSize: {
+    amount: Number,
+    unit: {type: String, default: 'g'}
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,8 +41,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+pantry: [foodSchema],
+  
 });
-
-const User = mongoose.model('User', userSchema);
+const User = new mongoose.model('User', userSchema);
 
 module.exports = User;
